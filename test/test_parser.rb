3c12d093100b8d8ad4f221a96bd7aa9245e9a539
dkallipo <67149964+dkallipo@users.noreply.github.com>
2025-08-08 16:21:44 +0300

    Create test_parser.rb

---
+++ test/test_parser.rb
@@ -0,0 +1,34 @@
+#!/usr/bin/env ruby
+# Test suite for GoogleKCParser module.
+# Dynamically defines tests for various Google Knowledge Carousel HTML files and compares
+# parser output against expected JSON results.
+
+require_relative '../lib/google_kc_parser'
+require 'test/unit'
+
+class GoogleKCParserTest < Test::Unit::TestCase
+  # Dynamically defines a test method for the given query
+  # Loads the corresponding HTML file and compares parser output with expected JSON.
+  def self.define_parser_test(query)
+    define_method("test_#{query.gsub(' ','_')}") do
+      html_file = query.gsub(' ','-') + '.html'
+      actual_results = GoogleKCParser.parse(html_file)
+
+      expected_results_file = File.join(FILES_DIR, html_file.gsub('.html','-expected.json'))
+      expected_results = JSON.parse(File.read(expected_results_file), symbolize_names:true)
+
+      assert_equal expected_results.count, actual_results.count, "results count"
+
+      expected_results.each_with_index do |expected_result, i|
+        [:name, :extensions, :link, :image].each do |field|
+          assert_equal expected_result[field], actual_results[i][field], "#{field} for result #{i}"
+        end
+      end
+    end
+  end
+
+  define_parser_test('van gogh paintings')
+  define_parser_test('dog breeds')
+  define_parser_test('michael jackson albums')
+  define_parser_test('stranger things cast')
+end
